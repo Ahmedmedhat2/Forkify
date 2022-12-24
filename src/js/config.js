c48838f4c4168c204d22e8d948bc4364abfde918
Ahmedmedhat2 <83168419+Ahmedmedhat2@users.noreply.github.com>
2022-12-24 17:33:15 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'ae570391-dc6b-42c8-9b86-20fb00788497';
 export const MODAL_CLOSE_SEC = 2.5;
